be1d6caaeea4218d7f3aee73b349b9d4b093b1ef
manishtmtmt <manisht.mt.mt@gmail.com>
2023-09-18 22:57:42 +0530

    deploy json-server and todos list app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
 export const fetchTodos = async () => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   return data.todos;
 };
 
 export const addTodo = async (todo) => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   const newData = [todo, ...data.todos];
 
-  await axios.post("http://localhost:3001/data", { todos: newData });
+  await axios.post("https://grumpy-hem-hen.cyclic.cloud/data", {
+    todos: newData,
+  });
 };
 
 export const isTodoCompleted = async (id) => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   let completed;
 
@@ -27,49 +29,55 @@ export const isTodoCompleted = async (id) => {
 };
 
 export const markTodoCompleted = async (id) => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   data.todos.forEach((todo) => {
     if (todo.id === id) todo.isCompleted = !todo.isCompleted;
   });
 
-  await axios.post("http://localhost:3001/data", { todos: data.todos });
+  await axios.post("https://grumpy-hem-hen.cyclic.cloud/data", {
+    todos: data.todos,
+  });
 };
 
 export const deleteTodo = async (id) => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   const updatedData = data.todos.filter((todo) => todo.id !== id);
 
-  await axios.post("http://localhost:3001/data", { todos: updatedData });
+  await axios.post("https://grumpy-hem-hen.cyclic.cloud/data", {
+    todos: updatedData,
+  });
 };
 
 export const countUncompletedTodo = async () => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   return data.todos.filter((todo) => !todo.isCompleted).length;
 };
 
 export const clearAllCompletedTodos = async () => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   const updatedData = data.todos.filter((todo) => !todo.isCompleted);
 
-  await axios.put("http://localhost:3001/data", { todos: updatedData });
+  await axios.put("https://grumpy-hem-hen.cyclic.cloud/data", {
+    todos: updatedData,
+  });
 };
 
 export const getActiveTodos = async () => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   return data.todos.filter((todo) => !todo.isCompleted);
 };
 
 export const getCompletedTodos = async () => {
-  const { data } = await axios.get("http://localhost:3001/data");
+  const { data } = await axios.get("https://grumpy-hem-hen.cyclic.cloud/data");
 
   return data.todos.filter((todo) => todo.isCompleted);
 };
 
 export const updateReOrderedTodos = async (todos) => {
-  await axios.put("http://localhost:3001/data", { todos });
+  await axios.put("https://grumpy-hem-hen.cyclic.cloud/data", { todos });
 };
